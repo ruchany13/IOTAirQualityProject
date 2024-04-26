578acfdce8cbe8a16c3df1b1e8e6d17055ff960c
ruchany13 <ruchany13@gmail.com>
2024-04-26 21:50:51 +0300

    add applicaiton and edit

--- SensorAPP/basic/app.py
+++ SensorAPP/basic/app.py
@@ -1,1 +1,24 @@
+import time
+import board
+import adafruit_dht
 
+
+sensor = adafruit_dht.DHT11(board.D4)
+
+while True:
+    try:
+        temperature_c = sensor.temperature
+        #temperature_f = temperature_c * (9 / 5) + 32
+        humidity = sensor.humidity
+        print("Sıcaklık={0:0.1f}ºC, Nem={1:0.1f}%".format(temperature_c, humidity))
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        sensor.exit()
+        raise error
+
+    time.sleep(1.0)
